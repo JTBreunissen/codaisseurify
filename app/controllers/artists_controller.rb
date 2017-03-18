b3608e4fa04c8edff93ed24f6da6c6263ddfc829
JTBreunissen <jantjarco@gmail.com>
2017-03-18 13:57:04 +0100

    created views for artist model

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -2,4 +2,36 @@ class ArtistsController < ApplicationController
   def index
     @artist = Artist.all
   end
+
+  def show
+    @artist = Artist.find(params[:id])
+  end
+
+  def new
+    @artist = Artist.new
+  end
+
+  def create
+    @artist = Artist.new(artist_params)
+
+    if @artist.save
+      image_params.each do |image|
+        @artist.photos.create(image: image)
+      end
+
+      redirect_to edit_event_path(@artist), notice: "Artist successfully created"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def artist_params
+    params.require(:artist).permit(:artistname, :origin, :website, :active)
+  end
+
+  def image_params
+    params[:images].present? ? params.require(:images) : []
+  end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,7 +1,7 @@
 class Song < ApplicationRecord
   belongs_to :artist
 
-  #validates :songtitle, presence: true
-  #validates :album, presence: true
-  #validates :releaseyear, presence: true, length: {maximum: 4, too_long: "Please enter a valid releaseyear"}
+  validates :songtitle, presence: true
+  validates :album, presence: true
+  validates :releaseyear, presence: true, length: {maximum: 4, too_long: "Please enter a valid releaseyear"}
 end
